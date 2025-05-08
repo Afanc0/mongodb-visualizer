acb247f022d70860270d6d774d0d0dc4358a2283
afanc0 <gianromani.gr@gmail.com>
2025-05-08 16:31:31 -0400

    add tool bar header in action panel

---
+++ src/components/action-panel/index.tsx
@@ -0,0 +1,14 @@
+import { ToolBarHeader } from "../tool-bar-header"
+
+export const ActionPanel = () => {
+    return (
+        <div className="flex flex-col gap-2 h-screen">
+            <div className="bg-white">
+                <ToolBarHeader />
+            </div>
+            <div className="flex-1 bg-white">
+                
+            </div>
+        </div>
+    )
+}
