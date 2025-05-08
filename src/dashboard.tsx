86b71459363dd0dd0bf8f42240e09382e8e74a74
afanc0 <gianromani.gr@gmail.com>
2025-05-08 13:14:08 -0400

    core communication -> get databases

--- src/dashboard.tsx
+++ src/dashboard.tsx
@@ -1,11 +1,25 @@
 import { DatabaseExplorer } from "./components/database-explorer"
+import { invoke } from "@tauri-apps/api/core"
+import { useEffect, useState } from "react"
+
+import { DatabaseInfo } from "./types/databases-info"
 
 function Dashboard() {
+
+    const [databases, setDatabases] = useState<DatabaseInfo[]>([])
+
+    useEffect(() => {
+        const getMongoDatabases = async () => setDatabases(await invoke("list_databases"))
+        getMongoDatabases()
+    }, [])
+
     return (
         <>
-            <div className="p-3 bg-[#3C3D37]">
-                <DatabaseExplorer />
-            </div>
+            <main>
+                <div className="p-3 bg-[#3C3D37]">
+                    <DatabaseExplorer itemList={databases}/>
+                </div>
+            </main>
         </>
     )
 }
