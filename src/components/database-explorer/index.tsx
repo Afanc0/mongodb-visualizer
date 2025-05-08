caac474f386d256777f2d1b0de2f1765e83891b8
afanc0 <gianromani.gr@gmail.com>
2025-05-08 13:30:43 -0400

    Sidebar -> renders the sidebar with menu items

--- src/components/database-explorer/index.tsx
+++ src/components/database-explorer/index.tsx
@@ -1,64 +1,40 @@
-import { Calendar, Home, Inbox, Search, Settings, Database, ChevronDown, ChevronRight, User2, ChevronUp } from "lucide-react"
 import { 
     Sidebar,
     SidebarContent, 
     SidebarGroup, 
-    SidebarFooter, 
-    SidebarHeader, 
     SidebarGroupLabel, 
-    SidebarMenuItem, 
-    SidebarMenuButton, 
     SidebarMenu, 
     SidebarGroupContent, 
     SidebarProvider,
-    SidebarMenuSub,
-    SidebarMenuSubItem,
     
 } from "../ui/sidebar"
-import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "../ui/collapsible"
+import { MenuItem } from "../menu-item"
 
-export const DatabaseExplorer = () => {
+import { DatabaseInfo } from "/@/types/databases-info"
+
+interface DatabaseExplorerProps {
+    itemList: DatabaseInfo[]
+}
+
+export const DatabaseExplorer = ({
+    itemList
+}: DatabaseExplorerProps) => {
     return (
         <SidebarProvider>
-            <Sidebar variant="floating">
+            <Sidebar variant="floating" className="w-[325px]">
                 <SidebarContent>
                     <SidebarGroup>
                         <SidebarGroupLabel>Mongo Databases</SidebarGroupLabel>
                         <SidebarGroupContent>
                             <SidebarMenu>
-
-                                <Collapsible className="group/collapsible">
-                                    <SidebarMenuItem>
-                                        <CollapsibleTrigger asChild>
-                                            <SidebarMenuButton className="group data-[state=open] flex items-center justify-between gap-2 w-full">
-                                                <div className="flex items-center gap-2">
-                                                    <Database className="w-4 h-4" color="gray" />
-                                                    <span>admin</span>
-                                                </div>
-
-                                                <div className="flex items-center gap-2">
-                                                    <span className="text-sm text-muted-foreground">40.00KiB</span>
-                                                    <ChevronRight className="h-4 w-4 transition-transform group-data-[state=open]:rotate-90" />
-                                                </div>
-                                            </SidebarMenuButton>
-                                        </CollapsibleTrigger>
-                                        <CollapsibleContent>
-                                            <SidebarMenuSub>
-                                                <SidebarMenuSubItem>
-                                                    <div className="flex flex-row">
-                                                        <span>categories</span>
-                                                    </div>
-                                                </SidebarMenuSubItem>
-                                            </SidebarMenuSub>
-                                        </CollapsibleContent>
-                                    </SidebarMenuItem>
-                                </Collapsible>
-                                
+                                {itemList.map((item) => (
+                                    <MenuItem key={item.name} item={item} />
+                                ))}
                             </SidebarMenu>
                         </SidebarGroupContent>
                     </SidebarGroup>
                 </SidebarContent>
-        </Sidebar>
-    </SidebarProvider>
+            </Sidebar>
+        </SidebarProvider>
     )
 }
