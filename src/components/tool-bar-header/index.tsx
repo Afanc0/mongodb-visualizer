3ed8e0cf574b66e19e2641b39cfa22ed4f52aa35
afanc0 <gianromani.gr@gmail.com>
2025-05-08 16:31:16 -0400

    add tool bar header

---
+++ src/components/tool-bar-header/index.tsx
@@ -0,0 +1,25 @@
+import { Edit, Plus, Trash } from "lucide-react"
+import { Button } from "../ui/button"
+import { Input } from "../ui/input"
+
+
+export const ToolBarHeader = () => {
+    return (
+        <div className="p-3 flex justify-between">
+            <div className="flex-1">
+                <Input type="text" placeholder="e.g., name='Car', model='Toyota'" />
+            </div>
+            <div className="flex gap-3 flex-1 justify-end">
+                <Button variant="outline" size="icon">
+                    <Edit />
+                </Button>
+                <Button variant="outline" size="icon">
+                    <Trash />
+                </Button>
+                <Button variant="outline" size="icon">
+                    <Plus />
+                </Button>
+            </div>
+        </div>
+    )
+}
