707bf6d49d38ddbe494af7de228d5e90eda60407
afanc0 <gianromani.gr@gmail.com>
2025-05-10 01:50:41 -0400

    added padding to search filter bar

--- src/components/tool-bar-header/index.tsx
+++ src/components/tool-bar-header/index.tsx
@@ -6,8 +6,8 @@ import { Input } from "../ui/input"
 export const ToolBarHeader = () => {
     return (
         <div className="p-3 flex justify-between">
-            <div className="flex-1">
-                <Input type="text" placeholder="e.g., name='Car', model='Toyota'" />
+            <div className="flex-1 px-3">
+                <Input type="text" placeholder="e.g., name='Car'; model='Toyota'" />
             </div>
             <div className="flex gap-3 flex-1 justify-end">
                 <Button variant="outline" size="icon">
