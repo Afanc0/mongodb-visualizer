491c62c9531722bce3ce6d3d790d12897e8339a3
afanc0 <gianromani.gr@gmail.com>
2025-05-10 01:05:00 -0400

    feat(renderer): add data table panel

--- src/components/data-table-panel/index.tsx
+++ src/components/data-table-panel/index.tsx
@@ -1,7 +1,38 @@
 
+import { useEffect, useState } from "react"
+import { DataTable } from "./data-table"
+import { Collection } from "/@/types/databases-info"
 
-export const DataTablePanel = () => {
+import { generateColumns } from "/@/utils/generic-column-generator"
+import { invoke } from "@tauri-apps/api/core"
+
+interface DataTablePanelProps {
+    fieldList: string[]
+    selectedCollection: Collection
+}
+
+export const DataTablePanel = ({
+    fieldList,
+    selectedCollection
+}: DataTablePanelProps) => {
+    const columns = generateColumns(fieldList)
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        const fetchRecords = async () => {
+            if (fieldList) {
+                setData(JSON.parse(await invoke("find_many", {
+                    db: selectedCollection.db,
+                    coll: selectedCollection.coll
+                })))
+            }
+        }
+        fetchRecords()
+    }, [fieldList])
+
     return (
-        <p>hi</p>
+        <div>
+            <DataTable columns={columns} data={data} />
+        </div>
     )
 }
