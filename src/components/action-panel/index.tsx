4d65ce36d1434bf63e36047258ab2656b52449f0
afanc0 <gianromani.gr@gmail.com>
2025-05-10 01:05:32 -0400

    feat(renderer): add action panel

--- src/components/action-panel/index.tsx
+++ src/components/action-panel/index.tsx
@@ -1,13 +1,31 @@
 import { ToolBarHeader } from "../tool-bar-header"
+import { DataTablePanel } from "../data-table-panel"
+import { CreateRecordPanel } from "../create-record-panel"
 
-export const ActionPanel = () => {
+import { Collection } from "/@/types/databases-info"
+
+interface ActionPanelProps {
+    fieldList: string[]
+    selectedCollection: Collection
+}
+
+export const ActionPanel = ({
+    fieldList,
+    selectedCollection
+}: ActionPanelProps) => {
     return (
-        <div className="flex flex-col gap-2 h-screen">
-            <div className="bg-white">
+        <div className="flex flex-col h-screen">
+            <div className="flex-none border-b">
                 <ToolBarHeader />
             </div>
-            <div className="flex-1 bg-white">
-                
+            <div className="flex-1 border-b overflow-auto">
+                <DataTablePanel 
+                    fieldList={fieldList}
+                    selectedCollection={selectedCollection}
+                />
+            </div>
+            <div className="flex-1 overflow-auto">
+                <CreateRecordPanel fieldList={fieldList} selectedCollection={selectedCollection}/>
             </div>
         </div>
     )
